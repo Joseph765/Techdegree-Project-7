dd87b2137068015391d7611aa615f74f4a051d27
Joseph Chauvin <joseph.chauvin102@gmail.com>
2019-11-22 12:36:43 -0600

    Removed unneeded import

--- techdegree-project-7/src/SearchForm.js
+++ techdegree-project-7/src/SearchForm.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { 
-    Link,
-    withRouter
-} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class SearchForm extends Component {
 
